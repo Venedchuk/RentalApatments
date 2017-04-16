f730635bab86958dd47cbeafb4efa40a687c1cf6
METEOR\andrii <sneyp67@gmail.com>
2017-04-16 19:30:32 +0300

    Add project files.

---
+++ RentalApatments/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentalApatments.Startup))]
+namespace RentalApatments
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
